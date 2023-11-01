e0cd390afa6cf8cab978132d596fa5e56baaf48b
Silvery <1364795855@qq.com>
2023-11-01 16:07:15 +0800

    后台管理整体布局

--- ims-frontend/src/views/home/IndexVue.vue
+++ ims-frontend/src/views/home/IndexVue.vue
@@ -1,13 +1,16 @@
 <script setup>
 
-import {Document, Location, Setting} from "@element-plus/icons-vue";
+import {Document, EditPen, House, Location, Notebook, Setting, User, Van} from "@element-plus/icons-vue";
+import router from "@/router";
 </script>
 
 <template>
   <div class="common-layout">
     <el-container style="height: 100vh">
       <el-aside width="200px" style="border-right: 1px solid #ccc">
-        <div style="height: 55px;display: flex; justify-content: center; align-items: center;font-weight: bold; font-size: 24px">
+        <div
+            style="height: 55px;display: flex; justify-content: center; align-items: center;font-weight: bold; font-size: 24px">
+          <el-icon style="margin-right: 3px"><Van /></el-icon>
           后台管理系统
         </div>
         <el-scrollbar class="menu" style="width: 100%; height: calc(100vh - 55px)">
@@ -15,95 +18,75 @@ import {Document, Location, Setting} from "@element-plus/icons-vue";
               default-active="2"
               class="el-menu-vertical-demo"
           >
-            <el-sub-menu index="1">
+            <el-menu-item>
               <template #title>
-                <el-icon><location /></el-icon>
-                <span>Navigator One</span>
+                <el-icon>
+                  <House/>
+                </el-icon>
+                <span>首页</span>
               </template>
-              <el-menu-item-group title="Group One">
-                <el-menu-item index="1-1">item one</el-menu-item>
-                <el-menu-item index="1-2">item two</el-menu-item>
-              </el-menu-item-group>
-              <el-menu-item-group title="Group Two">
-                <el-menu-item index="1-3">item three</el-menu-item>
-              </el-menu-item-group>
-              <el-sub-menu index="1-4">
-                <template #title>item four</template>
-                <el-menu-item index="1-4-1">item one</el-menu-item>
-              </el-sub-menu>
-            </el-sub-menu>
-            <el-sub-menu index="1">
-              <template #title>
-                <el-icon><location /></el-icon>
-                <span>Navigator One</span>
-              </template>
-              <el-menu-item-group title="Group One">
-                <el-menu-item index="1-1">item one</el-menu-item>
-                <el-menu-item index="1-2">item two</el-menu-item>
-              </el-menu-item-group>
-              <el-menu-item-group title="Group Two">
-                <el-menu-item index="1-3">item three</el-menu-item>
-              </el-menu-item-group>
-              <el-sub-menu index="1-4">
-                <template #title>item four</template>
-                <el-menu-item index="1-4-1">item one</el-menu-item>
-              </el-sub-menu>
-            </el-sub-menu>
-            <el-sub-menu index="1">
-              <template #title>
-                <el-icon><location /></el-icon>
-                <span>Navigator One</span>
-              </template>
-              <el-menu-item-group title="Group One">
-                <el-menu-item index="1-1">item one</el-menu-item>
-                <el-menu-item index="1-2">item two</el-menu-item>
-              </el-menu-item-group>
-              <el-menu-item-group title="Group Two">
-                <el-menu-item index="1-3">item three</el-menu-item>
-              </el-menu-item-group>
-              <el-sub-menu index="1-4">
-                <template #title>item four</template>
-                <el-menu-item index="1-4-1">item one</el-menu-item>
-              </el-sub-menu>
-            </el-sub-menu>
-            <el-sub-menu index="1">
-              <template #title>
-                <el-icon><location /></el-icon>
-                <span>Navigator One</span>
-              </template>
-              <el-menu-item-group title="Group One">
-                <el-menu-item index="1-1">item one</el-menu-item>
-                <el-menu-item index="1-2">item two</el-menu-item>
-              </el-menu-item-group>
-              <el-menu-item-group title="Group Two">
-                <el-menu-item index="1-3">item three</el-menu-item>
-              </el-menu-item-group>
-              <el-sub-menu index="1-4">
-                <template #title>item four</template>
-                <el-menu-item index="1-4-1">item one</el-menu-item>
-              </el-sub-menu>
-            </el-sub-menu>
-            <el-menu-item index="2">
-              <el-icon><icon-menu /></el-icon>
-              <span>Navigator Two</span>
             </el-menu-item>
-            <el-menu-item index="3" disabled>
-              <el-icon><document /></el-icon>
-              <span>Navigator Three</span>
-            </el-menu-item>
-            <el-menu-item index="4">
-              <el-icon><setting /></el-icon>
-              <span>Navigator Four</span>
-            </el-menu-item>
+            <el-sub-menu index="1">
+              <template #title>
+                <el-icon>
+                  <User/>
+                </el-icon>
+                <span>学生管理</span>
+              </template>
+              <el-menu-item index="1-1">学生信息</el-menu-item>
+              <el-menu-item index="1-2">学生选课</el-menu-item>
+              <el-menu-item index="1-3">学生成绩</el-menu-item>
+              <el-menu-item index="1-4">学生课表</el-menu-item>
+            </el-sub-menu>
+            <el-sub-menu index="2">
+              <template #title>
+                <el-icon>
+                  <Notebook/>
+                </el-icon>
+                <span>教师管理</span>
+              </template>
+              <el-menu-item index="1-1">教师信息</el-menu-item>
+              <el-menu-item index="1-2">教师课表</el-menu-item>
+            </el-sub-menu>
+            <el-sub-menu index="3">
+              <template #title>
+                <el-icon>
+                  <EditPen/>
+                </el-icon>
+                <span>考试管理</span>
+              </template>
+              <el-menu-item index="1-1">考试信息</el-menu-item>
+              <el-menu-item index="1-2">考试学生</el-menu-item>
+              <el-menu-item index="1-3">监考老师</el-menu-item>
+            </el-sub-menu>
+            <el-sub-menu index="4">
+              <template #title>
+                <el-icon>
+                  <setting/>
+                </el-icon>
+                <span>课程管理</span>
+              </template>
+              <el-menu-item index="1-1">课程信息</el-menu-item>
+              <el-menu-item index="1-2">任课老师</el-menu-item>
+              <el-menu-item index="1-3">选课情况</el-menu-item>
+            </el-sub-menu>
           </el-menu>
         </el-scrollbar>
       </el-aside>
       <el-container>
         <el-header style="height: 55px; border-bottom: 1px solid #ccc">
           <div style="width: 100%; text-align: right;">
-            <el-avatar
-                src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"
-            />
+            <el-dropdown>
+              <el-avatar style="width: 35px; height: 35px"
+                         src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"
+              />
+              <template #dropdown>
+                <el-dropdown-menu>
+                  <el-dropdown-item>个人中心</el-dropdown-item>
+                  <el-dropdown-item @click="router.push('/')">退出登录</el-dropdown-item>
+                </el-dropdown-menu>
+              </template>
+            </el-dropdown>
           </div>
         </el-header>
         <el-main style="background-color: rgba(246, 249, 248)">
